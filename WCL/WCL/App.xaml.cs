6dac2c68802eeb4177ef4c0eaec7976bfd7fe51c
Vakula Raman <vakularomans@gmail.com>
2024-05-16 10:59:16 +0300

    Add main view model, main template, and di

--- WCL/WCL/App.xaml.cs
+++ WCL/WCL/App.xaml.cs
@@ -1,14 +1,36 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WCL
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        public static IHost? AppHost { get; private set; }
 
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+            var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+            startupForm.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+            base.OnExit(e);
+        }
+    }
 }
---
+++ WCL/WCL/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WCL
+{
+    public class MainViewModel : ObservableObject
+    {
+    }
+}
